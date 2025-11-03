73989fcb190d73c24ba5c1480fd999298db6fd61
abrahamsil626 <silhernandez.223@gmail.com>
2025-11-03 11:50:24 -0500

    workflow done

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://abrahamsil626.github.io',
-  base: '/astrolanding_demo',
+  base: 'astrolanding_demo',
   vite: {
     plugins: [tailwindcss()]
   },
